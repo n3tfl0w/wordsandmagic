1425462ece2fbb84524f405a29e397d5552170bd
Dan O'Neill <dan.oneill@live.ie>
2016-01-22 00:23:08 +0000

    initial layout work for amp

---
+++ _plugins/amp_generator.rb
@@ -0,0 +1,5 @@
+# Create a generator that creates a second amp page for every post. Using a seperate template.
+
+# Import a file with array of html to amp html tags - like emoji plugin.
+
+# In those pages - replace html tags with amp versions of the same tags
