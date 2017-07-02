3be9cda5bec81932892ab24903b07acf4def17fb
Dan O'Neill <dan.oneill@live.ie>
2017-07-02 11:42:15 +0100

    small change

--- _plugins/amp-img-back.rb
+++ _plugins/amp-img-back.rb
@@ -2,8 +2,8 @@ Jekyll::Hooks.register :posts, :pre_render do |post, payload|
   docExt = post.extname.tr('.', '')
   # only process if we deal with a markdown file
   if payload['site']['markdown_ext'].include? docExt
-    newContent = post.content.gsub(/\<amp-img src="(.+)" alt="(.+)"/, '{% responsive_image path: \1 alt: \2  %}')
-    img_url = post.content.match(/\<amp-img src="(.+)" alt="(.+)"/)
+    newContent = post.content.gsub(/\<img src="(.+)" alt="(.+)"/, '{% responsive_image path: \1 alt: \2  %}')
+    img_url = post.content.match(/\<img src="(.+)" alt="(.+)"/)
     post.content = newContent
     post.data['imagea'] = img_url[1] if img_url != nil
   end
