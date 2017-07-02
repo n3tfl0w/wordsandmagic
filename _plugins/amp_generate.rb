2b5d1590dcfe5c7cdc2f1305a9100e5e92887df4
Dan O'Neill <dan.oneill@live.ie>
2017-07-02 12:26:34 +0100

    replacing custom amp generator with prebuilt gem

---
+++ _plugins/amp_generate.rb
@@ -0,0 +1,39 @@
+module Jekyll
+  # Defines the base class of AMP posts
+  class AmpPost < Page
+    def initialize(site, base, dir, post)
+      @site = site
+      @base = base
+      @dir = dir
+      @name = 'amp.html'
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'articleamp.html')
+      self.content               = post.content
+      self.data['body']          = (Liquid::Template.parse post.content).render site.site_payload
+
+      # Merge all data from post so that keys from self.data have higher priority
+      self.data = post.data.merge(self.data)
+
+      # Remove non needed keys from data
+      # Excerpt will cause an error if kept
+      self.data.delete('excerpt')
+      # Generating the page fails silently if page has a permalink and it is copied
+      # over to the AMP version
+      self.data.delete('permalink')
+
+      self.data['canonical_url'] = post.url
+      self.data['imagea']         = post.data['imagea']
+    end
+  end
+  # Generates a new AMP post for each post with article as the layout
+  class AmpGenerator < Generator
+    priority :low
+    def generate(site)
+      # dir = site.config['ampdir'] || 'amp'
+      site.posts.docs.each do |post|
+        next if post.data['layout'] == 'article'
+        site.pages << AmpPost.new(site, site.source, File.join(dir, post.id), post)
+      end
+    end
+  end
+end
