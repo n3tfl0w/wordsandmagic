79831739f1c4151a531e5cc64fe938560d7cfd3e
Dan O'Neill <cs.warrior+github@gmail.com>
2017-07-18 15:39:09 +0100

    Update gruntfile.js

--- gruntfile.js
+++ gruntfile.js
@@ -39,7 +39,7 @@ module.exports = function (grunt) {
                     width: 1298,
                     overwrite: true
                 },
-                src: 'images/*.JPG',
+                src: 'images/*',
                 dest: 'images/'
             }
         },
