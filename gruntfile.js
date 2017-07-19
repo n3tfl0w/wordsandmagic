0f46743f997d8267d92ea9bd38a0da06ce2425fc
Dan O'Neill <cs.warrior+github@gmail.com>
2017-07-19 12:27:26 +0100

    Update gruntfile.js

--- gruntfile.js
+++ gruntfile.js
@@ -37,7 +37,7 @@ module.exports = function (grunt) {
 	    target: {
       		options: {
 		    baseDir: '_site',
-		    ignore: ['.icons', '.logo', 'figure img']
+		    ignore: ['.icons', '.logo', 'figure img', 'iframe img']
 		},
       		files: [{
         	    expand: true,
